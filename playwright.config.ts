52c306650246c351ea346ea3cc04fcfeefd3f591
KanadeSneha <sneham27@gmail.com>
2026-02-13 13:05:21 +0000

    updated config.ts and yaml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
       ['line'], 
-      ['allure-playwright', { outputFolder: './allure-results' }]],
+      ['html', { outputFolder: 'playwright-report', open: 'never' }],
+      ['allure-playwright', { outputFolder: './allure-results' }],
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
